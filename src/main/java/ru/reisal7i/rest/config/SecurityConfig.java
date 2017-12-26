24ef7d7fb8f41363e903228e7243c37d516c6b22
Igor <igor.78@live.ru>
2017-12-26 23:15:00 +0300

    add oauth config

--- src/main/java/ru/reisal7i/rest/config/SecurityConfig.java
+++ src/main/java/ru/reisal7i/rest/config/SecurityConfig.java
@@ -5,20 +5,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@EnableWebMvc
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/", "/profile", "/login**", "/webjars/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-    }
-
-}
+//@Configuration
+//@EnableWebMvc
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .antMatcher("/**")
+//                .authorizeRequests()
+//                .antMatchers("/","/google/login", "/login**", "/webjars/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated();
+//    }
+//
+//}
